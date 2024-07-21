9402283b75f07a1dd731ab2b96394b1919409502
KrisnaMurti-21 <krisnamr27@gmail.com>
2024-07-21 21:38:53 +0700

    fixed style use bootstrap

--- script.js
+++ script.js
@@ -17,3 +17,95 @@ $(document).ready(function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var alergi = document.getElementById("alergi");
+  if (alergi) {
+    alergi.addEventListener("change", function () {
+      var value = this.value;
+      var jenisAlergiSection = document.getElementById("jenisAlergiSection");
+      if (value === "Ya") {
+        jenisAlergiSection.classList.remove("visually-hidden");
+      } else {
+        jenisAlergiSection.classList.add("visually-hidden");
+        document.getElementById("jenisAlergi").value = "";
+      }
+    });
+  }
+
+  var penyakitKronis = document.getElementById("penyakitKronis");
+  if (penyakitKronis) {
+    penyakitKronis.addEventListener("change", function () {
+      var value = this.value;
+      var jenisPenyakitSection = document.getElementById(
+        "jenisPenyakitSection"
+      );
+      if (value === "Ya") {
+        jenisPenyakitSection.classList.remove("visually-hidden");
+      } else {
+        jenisPenyakitSection.classList.add("visually-hidden");
+        document.getElementById("jenisPenyakit").value = "";
+      }
+    });
+  }
+
+  var perawatanMedis = document.getElementById("perawatanMedis");
+  if (perawatanMedis) {
+    perawatanMedis.addEventListener("change", function () {
+      var value = this.value;
+      var jenisPerawatanSection = document.getElementById(
+        "jenisPerawatanSection"
+      );
+      if (value === "Ya") {
+        jenisPerawatanSection.classList.remove("visually-hidden");
+      } else {
+        jenisPerawatanSection.classList.add("visually-hidden");
+        document.getElementById("jenisPerawatan").value = "";
+      }
+    });
+  }
+
+  var operasi = document.getElementById("operasi");
+  if (operasi) {
+    operasi.addEventListener("change", function () {
+      var value = this.value;
+      var jenisOperasiSection = document.getElementById("jenisOperasiSection");
+      if (value === "Ya") {
+        jenisOperasiSection.classList.remove("visually-hidden");
+      } else {
+        jenisOperasiSection.classList.add("visually-hidden");
+        document.getElementById("jenisOperasi").value = "";
+      }
+    });
+  }
+
+  var obatObatan = document.getElementById("obatObatan");
+  if (obatObatan) {
+    obatObatan.addEventListener("change", function () {
+      var value = this.value;
+      var jenisObatSection = document.getElementById("jenisObatSection");
+      if (value === "Ya") {
+        jenisObatSection.classList.remove("visually-hidden");
+      } else {
+        jenisObatSection.classList.add("visually-hidden");
+        document.getElementById("jenisObat").value = "";
+      }
+    });
+  }
+
+  var keterbatasanFisik = document.getElementById("keterbatasanFisik");
+  if (keterbatasanFisik) {
+    keterbatasanFisik.addEventListener("change", function () {
+      var value = this.value;
+      var jenisKeterbatasanSection = document.getElementById(
+        "jenisKeterbatasanSection"
+      );
+      if (value === "Ya") {
+        jenisKeterbatasanSection.classList.remove("visually-hidden");
+      } else {
+        jenisKeterbatasanSection.classList.add("visually-hidden");
+        document.getElementById("jenisKeterbatasan").value = "";
+      }
+    });
+  }
+});
